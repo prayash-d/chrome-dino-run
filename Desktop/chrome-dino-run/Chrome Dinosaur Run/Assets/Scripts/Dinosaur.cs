afa094ed01364502eab7ee03d79d8af24df6e6ce
prayash-d <prayash.d@outlook.com>
2023-01-01 14:15:59 -0500

    upload all files

---
+++ Desktop/chrome-dino-run/Chrome Dinosaur Run/Assets/Scripts/Dinosaur.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dinosaur : MonoBehaviour
+{
+    [SerializeField] private GameObject stand;
+    [SerializeField] private GameObject crouch;
+
+    Rigidbody2D rb;
+    bool isJumping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        isJumping = false; // dino isnt jumping in the beginning
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("space") && isJumping == false)
+        {
+            rb.velocity = new Vector3(0, 20, 0);
+            isJumping = true;
+        }
+
+        if (Input.GetKey(KeyCode.S) && isJumping == false)
+        {
+            crouch.SetActive(true);
+            stand.SetActive(false);
+        }
+        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJumping = false; //when dino collides with ground, jumping is now false
+    }
+}
---
+++ Desktop/chrome-dino-run/Chrome Dinosaur Run/Assets/Scripts/Dinosaur2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dinosaur2 : MonoBehaviour
+{
+
+    [SerializeField] private GameObject stand;
+    [SerializeField] private GameObject crouch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            stand.SetActive(true); //happens once you let go of S key
+            crouch.SetActive(false);      
+        }
+    }
+}
